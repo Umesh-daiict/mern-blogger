3d760f4ca30effa5a2585ccb3779e995270b633f
umesh <umeshsavaliya007@gmail.com>
2024-08-21 16:52:48 +0530

    update the user controller and post and comment routes

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,45 @@
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import User from '../models/user.model.js';
+
+export const test = (req, res) => {
+  res.json({ message: 'Server is Up!' });
+};
+
+
+export const getUser = async (req, res, next) => {
+    try {
+      const user = await User.findById(req.params.userId);
+      if (!user) {
+        return next(errorHandler(404, 'User not found'));
+      }
+      const { password, ...rest } = user._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  
+export const signout = (req, res, next) => {
+    try {
+      res
+        .clearCookie('access_token')
+        .status(200)
+        .json('User has been signed out');
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export const updateUser = async (req, res, next) => {
+  //TODO : create a update user
+}
+
+export const deleteUser = async (req, res, next) => {
+  //TODO : create a delete user
+};
+
+export const getUsers = async (req, res, next) => {
+  //TODO : create a all user for admin
+};
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
@@ -27,8 +30,16 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
-console.log("dsaj")
+app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
