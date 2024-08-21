8676e5ba430c65e5103276d3238f5cd3972267f5
umesh <umeshsavaliya007@gmail.com>
2024-08-21 17:36:14 +0530

    create the post and comment functionality

---
+++ backend/controllers/comment.controller.js
@@ -0,0 +1,25 @@
+import Comment from '../models/comment.model.js';
+
+export const createComment = async (req, res, next) => {
+//TODO : create it
+};
+
+export const getPostComments = async (req, res, next) => {
+  //TODO : create it
+};
+
+export const likeComment = async (req, res, next) => {
+ //TODO : create it
+};
+
+export const editComment = async (req, res, next) => {
+ //TODO : create it
+};
+
+export const deleteComment = async (req, res, next) => {
+ //TODO : create it
+};
+
+export const getcomments = async (req, res, next) => {
+//TODO : create it
+};
