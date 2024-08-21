f1ba0c2c17445d36924d434d472ef4ff92312610
umesh <umeshsavaliya007@gmail.com>
2024-08-21 19:58:23 +0530

    update the public route and design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 export default function App() {
   return (
-   <div>Hello</div>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
